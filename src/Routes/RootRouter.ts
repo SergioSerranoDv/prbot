1d98344065e910e9a0f7c9a3fe892d10350219eb
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-05-20 09:17:24 -0500

    refactor: Update RootRouter to log payload in console

--- src/Routes/RootRouter.ts
+++ src/Routes/RootRouter.ts
@@ -81,6 +81,7 @@ export class RootRouter {
     try {
       console.log("Sending message to Discord channel")
       const channelId = "1171241162804318310"
+      console.log(payload)
       const repository = payload.ref
       console.log(repository)
       const message = `New push event received on GitHub, repository: ${payload.repository.full_name}`
